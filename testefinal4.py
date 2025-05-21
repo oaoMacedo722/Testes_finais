6f1c2c0fcb4b726976a4c3589a56b4b2479782f8
Joao Macedo <joao.macedo.t0126442@edu.atec.pt>
2025-05-21 15:22:32 +0100

    c

---
+++ testefinal4.py
@@ -0,0 +1,28 @@
+#Criar um programa em Python para gerenciar um catálogo de até 100 livros. O programa deve permitir que o usuário realize as seguintes operações: 
+#Cadastrar livros: 
+#O programa deve permitir que o usuário adicione novos livros ao catálogo, informando o título, o autor e o ano de publicação. 
+#Procurar por livros: 
+#O usuário deve poder procurar por título ou autor. O programa deve exibir as posições onde o livro foi encontrado e mostrar o conteúdo correspondente. 
+#Excluir livros: 
+#O usuário deve poder excluir um livro a partir da sua posição no catálogo. A posição será dada pelo índice do livro na lista. 
+#Ordenar livros: 
+#O programa deve permitir que o usuário ordene os livros cadastrados por título ou autor. 
+#Listar todos os livros cadastrados: 
+#O programa deve listar todos os livros cadastrados, exibindo as informações de cada um (título, autor, ano). 
+#Sair do programa: 
+#O programa deve continuar em execução até que o usuário escolha explicitamente a opção de sair. 
+ 
+#Menu do Programa: 
+#O programa deve apresentar um menu com as seguintes opções: 
+#Adicionar novo livro 
+#Procurar por título ou autor 
+#Excluir livro 
+#Ordenar livros 
+#Listar livros 
+#Sair do Programa 
+ 
+#Requisitos: 
+#O programa deve permitir a execução contínua das operações, retornando sempre ao menu principal após cada ação. 
+#As operações de busca, exclusão e ordenação devem ser realizadas com base nas listas de títulos e autores. 
+#O programa deve garantir que não sejam cadastrados mais de 100 livros. 
+#Adicionar a possibilidade de verificar se o livro já está cadastrado antes de permitir o cadastro.
